eb35d8661b7d9a7fc7b354c4b8be45ca4d70a3f9
Xzar79 <seuuif79@gmail.com>
2020-06-17 09:40:20 +0900

    20200617김환
    .

---
+++ Assets/02. Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = transform.forward * speed;
+        Destroy(gameObject, 3f);
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // 충돌한 상대방 게임 오브젝트가 Player 태그를 가진 경우
+        if (other.tag == "Player")
+        { 
+            PlayerController playerController = other.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+
+
+    
+}
