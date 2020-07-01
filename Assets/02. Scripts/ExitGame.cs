1a2cc8f83983ec00aa2b13290d64a1d32bb86aac
Xzar79 <57945703+Xzar79@users.noreply.github.com>
2020-07-01 10:09:37 +0900

    BulletGame_Final2

---
+++ Assets/02. Scripts/ExitGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitGame : MonoBehaviour
+{
+    public bool isOn;
+    public Image image;
+
+    public void On()
+    {
+        isOn = !isOn;
+    }
+    void Start()
+    {
+        isOn = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            On();
+        }
+
+        if (isOn)
+            image.gameObject.SetActive(true);
+        else
+            image.gameObject.SetActive(false);
+    }
+
+    public void Onclick()
+    {
+        Application.Quit();
+    }
+}
